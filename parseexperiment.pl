d7e6cf8de4a717d97c426db1a63d0dbbec5a6413
ldfallas <ldfallas>
2015-01-10 20:40:32 -0600

    Created a separate file for tests

--- parseexperiment.pl
+++ parseexperiment.pl
@@ -1,3 +1,5 @@
+:- module(jsparser, [js_lex_string/2]).
+
 use_module(library(pure_io)).
 use_module(library(charsio)).
 
@@ -160,62 +162,6 @@ js_lex_string(Str, Toks) :-
 js_lex_string2(Str, Toks) :-
 	length(Str,Length),
 	phrase(toks2(Toks), [0|[[]|Str]], [Length,[]]).
-
-
-test_lex_var_decl :-
-   js_lex_string("var x;",
-		 [ tok(keyword, "var", _,_),
-		   tok(id, "x", _,_),
-		   tok(punctuator, ";",_, _) ]).
-
-test_lex_id_with_keyword_prefix_decl :-
-   js_lex_string("varx",
-		 [ tok(id, "varx", _,_)]).
-
-test_lex_numeric_literal :-
-   js_lex_string("234",
-		 [ tok(number, "234", _,_)]).
-
-
-test_lex_var_decl_with_init_with_comments :-
-   js_lex_string("var//uno
-x = 20;",
-		 [ tok(keyword, "var", _,_),
-		   tok(id, "x", _, [line_comment("uno",_)]),
-		   tok(punctuator, "=",_,_),
-		   tok(number,"20",_,_),
-		   tok(punctuator, ";",_, _) ]).
-
-test_lex_var_decl_with_init :-
-   js_lex_string("var x = 20;",
-		 [ tok(keyword, "var", _,_),
-		   tok(id, "x", _,_),
-		   tok(punctuator, "=",_,_),
-		   tok(number,"20",_,_),
-		   tok(punctuator, ";",_, _) ]).
-
-
-
-
-run_test(Test) :-
-	functor(Test, Name, _),
-	writef("Running "),
-	writef(Name),
-	(call(Test) -> writef(" PASS") ;
-	 (\+ call(Test)) -> writef(" FAILED")),
-	writef("\n").
-
-
-run_tests :-
-	run_test(test_lex_var_decl),
-	run_test(test_lex_id_with_keyword_prefix_decl ),
-	run_test(test_lex_numeric_literal),
-	run_test(test_lex_var_decl_with_init),
-	run_test(test_lex_var_decl_with_init_with_comments).
-
-
-
-
 
 
 
