b851a98566a8cfd12884508e6e2e04bc4c68f516
ldfallas <ldfallas>
2015-05-02 10:28:39 -0600

    Added code to generate test data

---
+++ multitest.pl
@@ -0,0 +1,75 @@
+:- use_module(jslexer).
+:- use_module(jsparser).
+/*:- use_module(tests).*/
+
+
+test(Pairs) :-
+   generate_test_operations([ ['*','/'],
+                              ['+','-'],
+                              ['<<','>>','>>>'],
+                              ['>','<','>=','<='],
+                              ['==', '!=', '===','!=='],
+                              ['&'],
+                              ['^'],
+                              ['|'],
+                              ['&&'],
+                              ['||']
+                              ], 
+                            ['x','y'], Pairs).
+
+testStr(Str) :-
+   test(Pairs),
+   member(Pair, Pairs),
+   test_nodes_to_string(Pair, Str).
+
+test_parse_stat(Str) :-
+   test(Pairs),
+   member(Pair, Pairs),
+   test_nodes_to_string(Pair, Str),
+   test_nodes_to_jsast(Pair, JsAst),
+   try_alternative(Str, JsAst).
+   /*((parse_js_expression_string(Str,JsAst) , !) ; writef("FAIL")).*/
+
+try_alternative(Str,JsAst) :-
+   ((parse_js_expression_string(Str,JsAst) , !) 
+     ; format("FAIL ~s~n",[Str])).
+
+test_nodes_to_string(Var,Str) :-
+   atom(Var),
+   atom_to_chars(Var, Str).
+
+test_nodes_to_string(op(Left, Operator , Right), Result) :-
+  atom_to_chars(Operator, OperatorStr), 
+  test_nodes_to_string(Left, LeftStr),
+  append(LeftStr, OperatorStr, TempLeft),
+  test_nodes_to_string(Right, RightStr),
+  append(TempLeft,RightStr, Result).
+
+test_nodes_to_jsast(Var, Ast) :-
+   atom(Var),
+   atom_to_chars(Var, Str),
+   Ast = js_identifier(Str, _).
+
+test_nodes_to_jsast(op(Left, Operator , Right), 
+       js_binary_operation( 
+            OperatorStr,
+            LeftAst,
+            RightAst,  _)) :-
+   atom_codes(Operator, OperatorStr),
+   test_nodes_to_jsast(Left, LeftAst),
+   test_nodes_to_jsast(Right, RightAst).
+
+
+generate_test_operations([],_,[]).
+generate_test_operations([Ops1|OpsRest],Vars, Pairs) :-
+  findall(Tuple, generating_test_operations(Ops1,Vars,Tuple),Pairs1), 
+  append(Vars,Pairs1,NewVars),
+  generate_test_operations(OpsRest, NewVars, Pairs2),
+  append(Pairs1, Pairs2, Pairs).
+
+generating_test_operations(Ops,[Var1|[Var2|Rest]],Pair1) :-
+  member(Op, Ops),
+  \+ Var1 = Var2, 
+  (Pair1 = op(Var1,Op,Var2))
+   ; generating_test_operations(Ops, Rest,Pair1). 
+	
--- tests.pl
+++ tests.pl
@@ -519,8 +519,14 @@ test_parse_equality2 :-
        "1 + 2 == 1 + 1 + 1",
         js_binary_operation( 
             "==", 
-            js_binary_operation("+", _ , _ , _) ,
-            _
+            js_binary_operation("+", 
+               js_literal(number,"1", _), 
+               js_literal(number,"2", _), 
+               _) ,
+            js_binary_operation("+", 
+               _, 
+               _, 
+               _)
             /*js_identifier("x", _),
             js_identifier("y", _)*/
             ,  _)).
@@ -619,3 +625,66 @@ run_tests :-
 
 /*member([X|_],X).
 member([_|Rest],X) :- member(Rest,X).*/
+
+
+
+
+test(Pairs) :-
+   generate_test_operations([['*','/'],['+','-'],
+['<<','>>','>>>'],['>','<','>=','<=']], ['x','y'], Pairs).
+
+testStr(Str) :-
+   test(Pairs),
+   member(Pair, Pairs),
+   test_nodes_to_string(Pair, Str).
+
+test_parse_stat(Str) :-
+   test(Pairs),
+   member(Pair, Pairs),
+   test_nodes_to_string(Pair, Str),
+   writef(Str),
+   test_nodes_to_jsast(Pair, JsAst),
+   ( ( (\+ parse_js_expression_string(Str,JsAst)), writef(" FAIL"))
+   /*  ; (writef(" FAIL") */
+     )).
+
+test_nodes_to_string(Var,Str) :-
+   atom(Var),
+   atom_to_chars(Var, Str).
+
+test_nodes_to_string(op(Left, Operator , Right), Result) :-
+  atom_to_chars(Operator, OperatorStr), 
+  test_nodes_to_string(Left, LeftStr),
+  append(LeftStr, OperatorStr, TempLeft),
+  test_nodes_to_string(Right, RightStr),
+  append(TempLeft,RightStr, Result).
+
+test_nodes_to_jsast(Var, Ast) :-
+   atom(Var),
+   atom_to_chars(Var, Str),
+   Ast = js_identifier(Str, _).
+
+test_nodes_to_jsast(op(Left, Operator , Right), 
+       js_binary_operation( 
+            OperatorStr,
+            LeftAst,
+            RightAst,  _)) :-
+   atom_codes(Operator, OperatorStr),
+   test_nodes_to_jsast(Left, LeftAst),
+   test_nodes_to_jsast(Right, RightAst).
+
+
+generate_test_operations([],_,[]).
+generate_test_operations([Ops1|OpsRest],Vars, Pairs) :-
+  findall(Tuple, generating_test_operations(Ops1,Vars,Tuple),Pairs1), 
+  append(Vars,Pairs1,NewVars),
+  generate_test_operations(OpsRest, NewVars, Pairs2),
+  append(Pairs1, Pairs2, Pairs).
+
+generating_test_operations(Ops,[Var1|[Var2|Rest]],Pair1) :-
+  member(Op, Ops),
+  \+ Var1 = Var2, 
+  (Pair1 = op(Var1,Op,Var2))
+   ; generating_test_operations(Ops, Rest,Pair1). 
+	
+
